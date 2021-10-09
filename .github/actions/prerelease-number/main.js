1964f837b21aaef88e0cc3a1a47042a94f624170
hdimon <hldimon@mail.ru>
2021-10-09 19:08:25 +0300

    Update main.js

--- .github/actions/prerelease-number/main.js
+++ .github/actions/prerelease-number/main.js
@@ -66,10 +66,39 @@ function main() {
   if (!env.INPUT_PRERELEASE_TYPE) fail(`ERROR: Input parameter prerelease_type is not defined.`);
         
   const branch = `${env.INPUT_BRANCH}`;
+  const branchName = `${env.INPUT_BRANCH##*/}`;
   const prereleaseType = `${env.INPUT_PRERELEASE_TYPE}`;
         
   console.log(`Branch: ${branch}`);
+  console.log(`Branch name: ${branchName}`);
   console.log(`Prerelease type: ${prereleaseType}`);
+        
+  //See if we've already generated the build number and are in later steps...
+  if (fs.existsSync(path)) {
+      let prereleaseNumber = fs.readFileSync(path);
+      console.log(`Prerelease number already generated in earlier jobs, using prerelease number ${prereleaseNumber}...`);
+      //Setting the output and a environment variable to new prerelease number...
+      fs.writeFileSync(process.env.GITHUB_ENV, `PRERELEASE_NUMBER=${prereleaseNumber}`);
+      console.log(`::set-output name=prerelease_number::${prereleaseNumber}`);
+      return;
+  }
+        
+  //Some sanity checking:
+  for (let varName of ['INPUT_TOKEN', 'GITHUB_REPOSITORY', 'GITHUB_SHA']) {
+      if (!env[varName]) {
+          fail(`ERROR: Environment variable ${varName} is not defined.`);
+      }
+  }
+        
+  /*request('GET', `/repos/${env.GITHUB_REPOSITORY}/git/refs/tags/${prefix}build-number-`, null, (err, status, result) => {
+      let nextBuildNumber, nrTags;
+          
+      if (status === 404) {
+          console.log('No prerelease-number ref available, starting at 1.');
+          nextBuildNumber = 1;
+          nrTags = [];
+      }
+  });*/
   
   return;
 }
