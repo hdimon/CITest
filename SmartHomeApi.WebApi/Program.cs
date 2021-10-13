1a077cd907c0ae2f223782766d0a51d623e4e62e
Dmitry <hldimon@mail.ru>
2021-10-13 11:53:33 +0300

    Fix Program build.

--- SmartHomeApi.WebApi/Program.cs
+++ SmartHomeApi.WebApi/Program.cs
@@ -15,7 +15,7 @@ namespace SmartHomeApi.WebApi
             var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false).Build();
 
             AppSettings settings = new AppSettings();
-            config.GetSection("AppSettings").Bind(settings);fg
+            config.GetSection("AppSettings").Bind(settings);
 
             if (!string.IsNullOrWhiteSpace(settings.ApiCulture))
             {
